b4dd396c6aaa2058c51a85a8d0a29d379c1bd427
Gilbert Mena <gilbert@myhibrid.com>
2012-11-15 03:56:16 -0700

    new features to test the class to store

--- tests/mocks/data/job/HelloWorld.php
+++ tests/mocks/data/job/HelloWorld.php
@@ -4,9 +4,24 @@ namespace li3_delayed_job\tests\mocks\data\job;
 
 class HelloWorld
 {
-  public function perform() {
-    $var = 'Hello World!';
-    echo $var;
+  private $name = 'Test Method';
+  private $constructVal;
+  
+  public function __construct($testID)
+  {
+    $this->constructVal = $testID;
     
+  }
+  
+  public function newMethod()
+  {
+    echo 'new' ."\r\n";
+  }
+  
+  public function perform() {
+    echo 'Started performing'."\r\n";
+    echo $this->name ."\r\n";
+    echo $this->constructVal."\r\n";
+    $this->newMethod();
   }
 }
